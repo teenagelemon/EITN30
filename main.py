5b5ba87b7ad5f036ea917e294cc247b3abcae9bc
Roberto <robertofeldt@hotmail.com>
2023-03-11 14:07:38 +0100

    cleaned working main from pi

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
+import subprocess
 import threading
 from tuntap import TunTap
 import struct
 import time
 from RF24 import RF24, RF24_PA_LOW, RF24_2MBPS, RF24_CRC_16
 import argparse
-global base #Ny, hämtar base från makefile 
-base = 0 # Ny standard värde så den försöker göra något iallafall 
+global role
 
 PSIZE = 30
 MAXBITS = 0xFFFF
 
-run_program = True
 w_radio = RF24(17, 0)
 r_radio = RF24(27, 60)
 role = 0
@@ -20,20 +19,15 @@ in_condition = threading.Condition()
 in_list = []
 out_condition = threading.Condition()
 out_list = []
-tun_condition = threading.Condition()
-
-
-role = base #Ny, så vi har något att skicka in i setup()
 
 
 def setup():
     if role == 0:
         tun.config(ip="192.168.2.1", mask="255.255.255.0")
-        # script for the rest
     elif role == 1:
         tun.config(ip="192.168.2.2", mask="255.255.255.0")
-        # script for the rest
-
+        subprocess.run(
+            'sudo ip route add default via 192.168.2.1 dev longge', shell=True)
     if not w_radio.begin():
         print("write radio was not started")
     if not r_radio.begin():
@@ -42,8 +36,8 @@ def setup():
     w_radio.setPALevel(RF24_PA_LOW)
     r_radio.setPALevel(RF24_PA_LOW)
 
-    w_radio.setRetries(5, 0)
-    r_radio.setRetries(5, 0)
+    w_radio.setRetries(15, 5)
+    r_radio.setRetries(15, 5)
 
     w_radio.setChannel(100)
     r_radio.setChannel(100)
@@ -54,16 +48,14 @@ def setup():
     w_radio.enableDynamicPayloads()
     r_radio.enableDynamicPayloads()
 
-    w_radio.setAutoAck()
-    r_radio.setAutoAck()
+    w_radio.setAutoAck(True)
+    r_radio.setAutoAck(True)
 
     w_radio.setCRCLength(RF24_CRC_16)
     r_radio.setCRCLength(RF24_CRC_16)
 
-    w_radio.stopListening()
     w_radio.openWritingPipe(addr[role])
-    r_radio.openReadingPipe(0, addr[not role])
-    r_radio.startListening()
+    r_radio.openReadingPipe(1, addr[not role])
 
     w_radio.flush_tx()
     r_radio.flush_rx()
@@ -72,7 +64,6 @@ def setup():
 def split_package(data: bytes) -> list:
     splits = []
     plen = len(data)
-
     if (plen <= 0):
         return
 
@@ -80,109 +71,79 @@ def split_package(data: bytes) -> list:
     while data:
         if (plen <= PSIZE):
             c = MAXBITS
-        splits.append(c.to_bytes(2, 'big'))
-        splits.append(data[:PSIZE])
+        splits.append(c.to_bytes(2, 'big') + data[:PSIZE])
         data = data[PSIZE:]
         plen = len(data)
         c += 1
-
     return splits
 
 
-def send(data: bytes): #Ändrade från byte till bytes 
+def send(data: bytes):
+    w_radio.stopListening()
     packages = split_package(data)
     for p in packages:
         w_radio.write(p)
 
 
 def receiver():
+    r_radio.startListening()
     buffer = []
     while True:
-        if run_program == False:
-            print("Closing receiver")
-            return
-        if (r_radio.available_pipe()): #Verkar ej tycka om det här 
+        has_payload, pipeNbr = r_radio.available_pipe()
+        if has_payload:
             psize = r_radio.getDynamicPayloadSize()
-            if (psize < 1):
-                pack = r_radio.read(psize)
-                c = int.from_bytes(pack[:2], 'big')
-                buffer.append(pack[2:])
-                if c == MAXBITS:
-                    packet = b''.join(buffer)
-                    buffer.clear()
-                    with out_condition:
-                        out_list.append(packet)
-                        out_condition.notify_all()
+            pack = r_radio.read(psize)
+            c = int.from_bytes(pack[:2], 'big')
+            buffer.append(pack[2:])
+            if c == MAXBITS:
+                packet = b''.join(buffer)
+                buffer.clear()
+                with out_condition:
+                    out_list.append(packet)
+                    out_condition.notify()
 
 
 def transmitter():
     while True:
-        if run_program == False:
-            print("Closing receiver")
-            return
         with in_condition:
             while not len(in_list) > 0:
                 in_condition.wait()
             packet = in_list.pop()
-            in_condition.notify_all()
         send(packet)
 
 
 def tun_trasmitter():
     while True:
-        if run_program == False:
-            print("Closing receiver")
-            return
         with out_condition:
             while not len(out_list) > 0:
                 out_condition.wait()
             packet = out_list.pop()
         tun.write(packet)
-        out_condition.notify_all()
-        tun_condition.notify_all()
 
 
 def tun_receiver():
     while True:
-        if run_program == False:
-            print("Closing receiver")
-            return
-        with tun_condition:
-            buffer = tun.read()
-            if len(buffer):
-                send(buffer)
-        tun_condition.notify_all()
-
-#Vet ej tanken med detta tbh, har nog missat något 
-#def run():
-#    role = input("base = 0, mobile = 1")
-#    setup()
-#    if (role == 0):
-#        base()
-#    elif (role == 1):
-#        mobile()
-#    else:
-#        print("wrong role")
-#        return 
-
+        buffer = tun.read()
+        if len(buffer):
+            send(buffer)
 
 
 if __name__ == "__main__":
-    
-    #Detta är för att kunna läsa från makefilen 
+
     parser = argparse.ArgumentParser(description='NRF24L01+ test')
-    parser.add_argument('--base', dest='base', type=int, default=0, help='Whether the unit is mobile or the base station', choices=range(0,2))
+    parser.add_argument('--role', dest='role', type=int, default=0,
+                        help='Whether the unit is mobile or the base station', choices=range(0, 2))
     args = parser.parse_args()
-    base = args.base
-    
+    role = args.role
+    setup()
     radio_s = threading.Thread(target=transmitter, args=())
     radio_r = threading.Thread(target=receiver, args=())
     tun_s = threading.Thread(target=tun_trasmitter, args=())
     tun_r = threading.Thread(target=tun_receiver, args=())
-    
 
     radio_s.start()
     radio_r.start()
+    time.sleep(0.5)
     tun_s.start()
     tun_r.start()
 
@@ -190,7 +151,3 @@ if __name__ == "__main__":
     radio_r.join()
     tun_s.join()
     tun_r.join()
-    setup() #La till denna så den går in i setup()
-    exit_input = input("input anything to exit")
-    if (len(exit_input)):
-        close_threads = 1
