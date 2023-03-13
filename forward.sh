7da9ed579fc2f68cddbabdec9e4b5f926b6a2653
David Ekblad <da6135ek-s@student.lund.se>
2023-03-13 15:51:29 +0100

    fixed the shell scripts so it says longge not tun0

--- forward.sh
+++ forward.sh
@@ -1,4 +1,4 @@
 sudo iptables -F
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo iptables -A FORWARD -i eth0 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i tun0 -o eth0 -j ACCEPT
+sudo iptables -A FORWARD -i eth0 -o longge -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i longge -o eth0 -j ACCEPT
--- iptables_mobile.sh
+++ iptables_mobile.sh
@@ -3,6 +3,6 @@ sudo iptables -P INPUT DROP
 sudo iptables -P FORWARD DROP
 sudo iptables -P OUTPUT DROP
 
-sudo iptables -A INPUT -i tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -o tun0 -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -i longge -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -o longge -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
 
